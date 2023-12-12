842709bdc5d6a9b6ab3ad0e1e5fe74b0e7d41eb1
snakada <nakada.family@gmail.com>
2023-12-12 15:24:45 +0900

    quasar working

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/test',
+      name: 'test',
+      component: () => import('../views/test01.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
