b6056b432619ce49902fcfac8b47a70a7a2562e6
snakada <nakada.family@gmail.com>
2023-12-13 15:34:06 +0900

    quasar*storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,19 @@
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/animate/fadeInUp.css'
+import '@quasar/extras/animate/fadeOutDown.css'
+import '@quasar/extras/animate/fadeInRight.css'
+import '@quasar/extras/animate/fadeOutRight.css'
+
+import 'quasar/dist/quasar.css'
 import type { Preview } from '@storybook/vue3'
+import { setup } from '@storybook/vue3'
+
+import { Quasar } from 'quasar'
+
+setup((app) => {
+  app.use(Quasar, {})
+})
 
 const preview: Preview = {
   parameters: {
