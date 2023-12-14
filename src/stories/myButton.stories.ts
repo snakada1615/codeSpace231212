18e9938b9073fa7406d86d941b5a70c9dab062f8
shunichi <khf04440@nifty.com>
2023-12-14 10:09:31 +0900

    my first story

---
+++ src/stories/myButton.stories.ts
@@ -0,0 +1,24 @@
+// Replace vue3 with vue if you are using Storybook for Vue 2
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import Button from 'src/components/test/myButtons.vue'
+
+const meta: Meta<typeof Button> = {
+  component: Button
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  name: 'I am the primary',
+  render: () => ({
+    components: { Button },
+    template: '<Button />'
+  })
+}
