bc17c213ea41516708a87ef87c94efed83f1d06d
shunichi <khf04440@nifty.com>
2023-12-15 10:40:18 +0900

    first storybook trial for listitem

---
+++ src/components/test/listItem.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+interface diversityState {
+  name: string
+  status: boolean
+}
+
+defineProps({
+  diversityStatus: {
+    type: Array as PropType<diversityState[]>,
+    default: () => []
+  }
+})
+const setBgColor = (val: boolean) => {
+  return val ? 'bg-red-4' : 'bg-blue-4'
+}
+</script>
+
+<template>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-card bordered>
+      <q-card-section class="q-mb-xs bg-green-4">
+        <div class="text-h6 q-py-xs">Dietary diversity</div>
+      </q-card-section>
+      <q-card-section class="q-mt-xs">
+        <q-list bordered separator dense class="rounded">
+          <q-item
+            clickable
+            v-ripple
+            v-for="diversityState in diversityStatus"
+            :key="diversityState.name"
+            :class="setBgColor(diversityState.status)"
+          >
+            <q-item-section>{{ diversityState.name }}</q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
---
+++ src/stories/listItem.stories.ts
@@ -0,0 +1,43 @@
+// Replace vue3 with vue if you are using Storybook for Vue 2
+import type { Meta, StoryObj } from '@storybook/vue3'
+import listItem from 'src/components/test/ListItem.vue'
+interface diversityState {
+  name: string
+  status: boolean
+}
+
+const diversityStatus: diversityState[] = [
+  {
+    name: 'Starchy roots, tubers and their products',
+    status: true
+  },
+  {
+    name: 'Legumes and their products',
+    status: false
+  },
+  {
+    name: 'Nuts, seeds and their products',
+    status: true
+  }
+]
+
+const meta: Meta<typeof listItem> = {
+  component: listItem
+}
+
+export default meta
+
+type Story = StoryObj<typeof listItem>
+
+export const Secondary: Story = {
+  render: (args) => ({
+    components: { listItem },
+    setup() {
+      return { args }
+    },
+    template: '<listItem v-bind="args" />'
+  }),
+  args: {
+    diversityStatus
+  }
+}
