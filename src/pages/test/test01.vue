d234d3c4b3721d15bf317ad8a36a2e82b04fc89f
shunichi <khf04440@nifty.com>
2024-01-23 21:33:30 +0300

    一時保管（quasar-sass問題）

--- src/pages/test/test01.vue
+++ src/pages/test/test01.vue
@@ -1,9 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import * as myVal from '../../models/MyInterface'
-import fireFunc from '../../models/fakerFunc'
+import { fireFunc } from '../../models/fireFunctions'
 import { computed, ref, type PropType } from 'vue'
-const data01 = ref([
+
+interface DatType {
+  firestName: string
+  lastName: string
+  character: {
+    hobby: string
+    movie: {
+      top: string
+      '2nd': string
+    }
+  }
+}
+
+interface DatTypeArray extends Array<DatType> {}
+
+const data01 = ref<DatTypeArray>([
   {
     firestName: 'nanmo',
     lastName: 'naiko',
@@ -27,10 +42,21 @@ const data01 = ref([
     }
   }
 ])
+
+function saveDat() {
+  fireFunc.fireSetTyped<DatTypeArray>('test', 'setMerge', data01.value)
+}
+
+function modifyDat() {
+  data01.value[1].character.movie['2nd'] = 'ハゲ親父'
+  fireFunc.fireSetMergeTyped<DatTypeArray>('test', 'setMerge', data01.value)
+}
 </script>
 <template>
   <q-card>
     <div>halo</div>
     <div>{{ data01 }}</div>
   </q-card>
+  <q-btn label="here" @click="saveDat" />
+  <q-btn label="there" @click="modifyDat" />
 </template>
