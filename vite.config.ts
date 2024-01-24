e17776a4aee694d30669692408702dc7e37adfb0
shunichi <khf04440@nifty.com>
2024-01-24 16:40:39 +0900

    pwaインストール済み

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +21,8 @@ export default defineConfig({
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
