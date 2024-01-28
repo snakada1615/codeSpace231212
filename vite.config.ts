4d82dda817602c6e680ac3666e97de1f316d11e0
shunichi <khf04440@nifty.com>
2024-01-28 20:20:40 +0900

    一時保管

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { VitePWA } from 'vite-plugin-pwa'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,10 +17,6 @@ export default defineConfig({
       template: { transformAssetUrls }
     }),
     vueJsx(),
-    visualizer({
-      open: true, // This will open the visualizer report in your browser after the build
-      filename: 'dist/stats.html' // Specifies the output file for the report
-    }),
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
