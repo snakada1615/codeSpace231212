b6c1835ab7e79715d2b45ca1ffface291e872d81
shunichi <khf04440@nifty.com>
2024-03-12 08:36:49 +0900

    0312@home

--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -149,6 +149,7 @@ export const useProjectData = defineStore('prjData', {
       console.log('updates...')
       console.log(updates)
 
+      await fireFunc.fireUpdateTyped(collectionName, docId, updates, 'piniaStatePartial')
       // Your Firestore document reference, now with a converter applied
       // const documentRef = doc(collectionName, docId).withConverter(yourStoreConverter)
       // await updateDoc(documentRef, updates)
